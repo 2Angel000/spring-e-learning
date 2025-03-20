b8679e6f27f12f1d510f5bbb2ca9f42dbcc88f32
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2025-03-19 22:05:06 -0600

    Back-Front unidos

--- src/main/java/com/capacitamx/config/WebConfig.java
+++ src/main/java/com/capacitamx/config/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhots:5173")
-                        .allowedMethods("GET","POST","PUT","DELETE");
+                        .allowedOrigins("http://localhots:5173", "http://localhost:3000")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowCredentials(true);
             }
         }  ;
     }
