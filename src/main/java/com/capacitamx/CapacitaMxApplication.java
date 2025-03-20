ae69bdb67dc6073feb0fe7bc6eba657872246c31
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2025-03-19 21:30:35 -0600

    Proyecto inicial

---
+++ src/main/java/com/capacitamx/CapacitaMxApplication.java
@@ -0,0 +1,13 @@
+package com.capacitamx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitaMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitaMxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/capacitamx/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.capacitamx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhots:5173")
+                        .allowedMethods("GET","POST","PUT","DELETE");
+            }
+        }  ;
+    }
+
+}
